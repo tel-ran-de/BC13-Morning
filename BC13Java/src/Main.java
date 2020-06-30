7260ed47c2e7abaf42166ff8e329729fccb74eff
andron13 <andron13@gmail.com>
2020-06-30 09:44:13 +0200

    Lesson - 01 Software(JDK, IDEA) installation, Java Hello World

---
+++ BC13Java/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("() {} [] /\\ '' \" ~ +-/* & ^ <> | ");
+        System.out.println(" Домашнее задание: научить пальчики вызывать различные смешные символы");
+        System.out.println();
+        customerInfo();
+        thankYou();
+    }
+
+    public static void customerInfo() {
+        System.out.println("С вас 3,60€!");
+    }
+
+    public static void thankYou() {
+        System.out.println("Спасибо за покупку!");
+        System.out.println("А здесь будет выполняться арифметическая операция 3+5" + 5 + 3);
+        System.out.print("Меня зовут Андрей \t");
+        System.out.print("Но я хочу \nбыть Сергеем");
+        System.out.print("ln = New Line");
+        System.out.print("\n");
+        System.out.print("\u0423\u0440\u0430 \n");
+    }
+}
