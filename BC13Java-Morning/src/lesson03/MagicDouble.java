e53b7e45bbb0ae7681839758b753e3745022a1de
andron13 <andron13@gmail.com>
2020-07-01 12:55:14 +0200

    lesson -03

---
+++ BC13Java-Morning/src/lesson03/MagicDouble.java
@@ -0,0 +1,31 @@
+package lesson03;
+
+public class MagicDouble {
+
+    public static void main(String[] args) {
+        int min = 30;
+        int max = 100;
+        int result0 = (int) ((Math.random() * ((max - min) + 1)) + min);
+        int result1 = (int) ((Math.random() * ((max - min) + 1)) + min);
+        int result2 = (int) ((Math.random() * ((max - min) + 1)) + min);
+        int result3 = (int) ((Math.random() * ((max - min) + 1)) + min);
+        int result4 = (int) ((Math.random() * ((max - min) + 1)) + min);
+        int result5 = (int) ((Math.random() * ((max - min) + 1)) + min);
+        int result6 = (int) ((Math.random() * ((max - min) + 1)) + min);
+        int result7 = (int) ((Math.random() * ((max - min) + 1)) + min);
+        int result8 = (int) ((Math.random() * ((max - min) + 1)) + min);
+        int result9 = (int) ((Math.random() * ((max - min) + 1)) + min);
+        System.out.println(result0);
+        System.out.println(result1);
+        System.out.println(result2);
+        System.out.println(result3);
+        System.out.println(result4);
+        System.out.println(result5);
+        System.out.println(result6);
+        System.out.println(result7);
+        System.out.println(result8);
+        System.out.println(result9);
+        double test = Math.random();
+        System.out.println(test);
+    }
+}
