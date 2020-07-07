34a8a13426fcad264a4579eef489197f5a587486
andron13 <andron13@gmail.com>
2020-07-07 10:02:49 +0200

    l5

---
+++ BC13Java-Morning/src/SwitchDemoCalendar.java
@@ -0,0 +1,50 @@
+
+public class SwitchDemoCalendar {
+    public static void main(String[] args) {
+        int monthDigit = 10;
+        String monthString = "";
+
+        switch (monthDigit) {
+            case 1:
+                monthString = "Январь";
+                break;
+            case 2:
+                monthString = "Февраль";
+                break;
+            case 3:
+                monthString = "Март";
+                break;
+            case 4:
+                monthString = "Апрель";
+                break;
+            case 5:
+                monthString = "Май";
+                break;
+            case 6:
+                monthString = "Июнь";
+                break;
+            case 7:
+                monthString = "Июль";
+                break;
+            case 8:
+                monthString = "Август";
+                break;
+            case 9:
+                monthString = "Сентябрь";
+                break;
+            case 10:
+                monthString = "Октябрь";
+                break;
+            case 11:
+                monthString = "Ноябрь";
+                break;
+            case 12:
+                monthString = "Декабрь";
+                break;
+            default:
+                monthString = "Invalid month - " + monthDigit;
+                break;
+        }
+        System.out.println(monthString);
+    }
+}
