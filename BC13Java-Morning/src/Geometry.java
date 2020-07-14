6ec17e697744c98ea80450b51b38b5f3c7c4872a
andron13 <andron13@gmail.com>
2020-07-14 11:04:09 +0200

    geometry

---
+++ BC13Java-Morning/src/Geometry.java
@@ -0,0 +1,55 @@
+public class Geometry {
+    /*
+     *
+     **
+     ***
+     ****
+     *****
+     ******
+     *******
+     ********
+     *********
+     **********
+     */
+    public static void main(String[] args) {
+        // first("*", 10);
+        //second("*", 10);
+        third("*", 10);
+    }
+
+    public static void first(String divider, int countLines) {
+        String line = "";
+        for (int i = 0; i < countLines; i++) {
+            line = line + divider;
+            System.out.println(line);
+        }
+    }
+
+    public static void second(String divider, int countLines) {
+        String line = "";
+        for (int i = 0; i < countLines; i++) {
+            line = line + divider;
+        }
+        for (int i = 0; i < countLines; i++) {
+            line = line.substring(0, line.length() - 1);
+            //line = line.substring(line.length() - 1);
+            System.out.println(line);
+        }
+    }
+
+    public static void third(String divider, int countLines) {
+        String lineSpace = "";
+        String lineStar = "";
+
+        for (int i = 0; i < countLines; i++) {
+            lineSpace = lineSpace + " ";
+        }
+
+        for (int i = 0; i < countLines; i++) {
+            lineSpace = lineSpace.substring(0, lineSpace.length() - 1);
+            //line = line.substring(line.length() - 1);
+            lineStar = lineStar + divider;
+            System.out.println(lineSpace + lineStar);
+        }
+    }
+}
--- BC13Java-Morning/src/Main.java
+++ BC13Java-Morning/src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-
+        String name = "Василий";
+        name = name.substring(name.length() - 1);
+        System.out.println(name);
     }
 }
