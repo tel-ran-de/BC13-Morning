0cde371bd11a845ace937f9a6cae329eee81836f
andron13 <andron13@gmail.com>
2020-07-17 12:33:35 +0200

    String 01

--- BC13Java-Morning/src/ExercisesString01.java
+++ BC13Java-Morning/src/ExercisesString01.java
@@ -24,4 +24,53 @@ public class ExercisesString01 {
         return word.substring(0, word.length() / 2);
     }
 
+    public static String nonStart(String a, String b) {
+        return a.substring(1) + b.substring(1);
+    }
+
+    public static String theEnd(String s, boolean front) {
+        if (front) {
+            return s.substring(0, 1);
+        } else {
+            return s.substring(s.length() - 1);
+        }
+    }
+
+    public static boolean endsLy(String str) {
+        return str.endsWith("ly");
+    }
+
+    public static boolean endsLyDos(String str) {
+        String reference = "ly";
+        int length = str.length();
+        if (length < 2) {
+            return false;
+        }
+        return reference.equals(str.substring(str.length() - 2));
+    }
+
+    public static String middleThree(String s) {
+        int middle = s.length() / 2;
+        return s.substring(middle - 1, middle + 2);
+    }
+
+    public static String lastChars(String a, String b) {
+        if (a.length() < 1) {
+            a = "@";
+        }
+        if (b.length() == 0) {
+            b = "@";
+        }
+        return a.substring(0, 1) + b.substring(b.length() - 1);
+    }
+
+    public static String seeColor(String s) {
+        String red = "red";
+        String blue = "blue";
+        if (s.substring(0, 3).equals(red)) {
+            return red;
+        } else if (s.substring(0, 4).equals(blue)) {
+            return blue;
+        } else return "";
+    }
 }
--- BC13Java-Morning/src/Main.java
+++ BC13Java-Morning/src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-
+        String name = "Василий";
+        String name2 = "василий";
+        System.out.println(name.toLowerCase().equals(name2.toLowerCase()));
     }
 }
