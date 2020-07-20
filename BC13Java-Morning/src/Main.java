0c63051137b40a5c736f5011911301fe9838f50a
andron13 <andron13@gmail.com>
2020-07-20 13:24:34 +0200

    18

--- BC13Java-Morning/src/Main.java
+++ BC13Java-Morning/src/Main.java
@@ -1,7 +1,45 @@
 public class Main {
     public static void main(String[] args) {
-        String name = "Василий";
-        String name2 = "василий";
-        System.out.println(name.toLowerCase().equals(name2.toLowerCase()));
+        //mySimpleFor();
+        // mySimpleWhile();
+        // mySimpleDoWhile();
+        myNotSimpleWhile();
+    }
+
+    private static void myNotSimpleWhile() {
+        int i = 0;
+        while (true) {
+            System.out.println(i);
+            i++;
+            if (i > 20) {
+                break;
+            }
+            // nested loops
+            for (int j = 0; j < 20; j++) {
+                System.out.println(j);
+            }
+        }
+    }
+
+    private static void mySimpleDoWhile() {
+        int i = 0;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i < 0);
+    }
+
+    private static void mySimpleWhile() {
+        int i = 0;
+        while (i < 20) {
+            System.out.println(i);
+            i++;
+        }
+    }
+
+    private static void mySimpleFor() {
+        for (int i = 0; i < 20; i++) {
+            System.out.println(i);
+        }
     }
 }
