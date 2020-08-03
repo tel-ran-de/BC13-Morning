c60dc4bcf634da9e25969301f6be71f89b2d5fb4
andron13 <andron13@gmail.com>
2020-08-03 19:01:32 +0200

    Array - first Day

--- BC13Java-Morning/src/ArrayDoubleInit.java
+++ BC13Java-Morning/src/ArrayDoubleInit.java
@@ -1,6 +1,27 @@
 public class ArrayDoubleInit {
     public static void main(String[] args) {
         double[] doubleArray01 = new double[10];
-        
+        double[] doubleArray02 = fillMyDoubleArray(doubleArray01);
+        printDoubleArray(doubleArray02);
+    }
+
+    public static double myRound(double d) {
+        int temp = (int) (d * 100);
+        return (double) temp / 100;
+    }
+
+    private static double[] fillMyDoubleArray(double[] myArray) {
+        for (int i = 0; i < myArray.length; i++) {
+            myArray[i] = myRound(Math.random());
+        }
+        return myArray;
+    }
+
+    private static void printDoubleArray(double[] input) {
+        System.out.println("Массив дробей, длиной - " + input.length);
+        for (double d : input) {
+            System.out.print(d + " ");
+        }
+        System.out.println();
     }
 }
---
+++ BC13Java-Morning/src/StringArrayInit.java
@@ -0,0 +1,37 @@
+public class StringArrayInit {
+    public static void main(String[] args) {
+        String[] str = new String[10];
+        // myPrintForStringArray(str);
+        myPrintForStringArray(createRandomStringArray(10));
+    }
+
+
+    private static String[] createRandomStringArray(int n) {
+        String[] result = new String[n];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = generateString(10).toLowerCase();
+        }
+        return result;
+    }
+
+    private static void myPrintForStringArray(String[] str) {
+        System.out.println("Строковый массив, длиной - " + str.length);
+        for (int i = 0; i < str.length; i++) {
+            System.out.print(str[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static String generateString(int n) {
+        String result = "";
+        for (int i = 0; i < n; i++) {
+            result = result + oneLetter();
+        }
+        return result;
+    }
+
+    private static char oneLetter() {
+        String source = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        return source.charAt((int) (Math.random() * source.length()));
+    }
+}
