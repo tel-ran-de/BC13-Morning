0b3ccb076022c0779bde99ad8013fff86152489f
andron13 <andron13@gmail.com>
2020-08-14 12:17:29 +0200

    40, tiles

---
+++ BC13Java-Morning/src/tilesExercises/Main.java
@@ -0,0 +1,10 @@
+package tilesExercises;
+
+public class Main {
+    public static void main(String[] args) {
+        double roomWidth = 5.5; // метры
+        double roomLength = 7.5; // метры
+        double tilesPrice = 3.0;
+        TilesApplication.start(roomWidth, roomLength, tilesPrice);
+    }
+}
