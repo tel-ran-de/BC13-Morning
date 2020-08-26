40b1f30d88b4645de409addb1fba997a4cfc54c0
andron13 <andron13@gmail.com>
2020-08-26 10:16:13 +0200

    48, insert sort with sout

--- BC13Java-Morning/src/AnotherNewDayAndNewSort.java
+++ BC13Java-Morning/src/AnotherNewDayAndNewSort.java
@@ -9,18 +9,30 @@ public class AnotherNewDayAndNewSort {
         int[] array = {22, 100, 55, 33, 88, 7};
         System.out.println(Arrays.toString(insertionSort(array)));
     }
+
     private static int[] insertionSort(int[] input) {
         for (int i = 1; i < input.length; i++) {
             int newElement = input[i];  // временная переменная, которая хранит значение
             // цикл начался не с нуля, а с единицы. Location предыдущее место от элемента массива.
             int location = i - 1;
+
+            System.out.println("i или круг = " + i);
+            System.out.println(newElement);
+            System.out.println(input[location]);
+            System.out.println("**");
+
             // Пока значение от ЛОКАШИОН больше значения элемента в проверке
             while (location >= 0 && input[location] > newElement) {
+                System.out.println("Находимся в цикл while при i = " + i);
+                System.out.println(newElement);
+                System.out.println(input[location]);
+                System.out.println("++");
+
                 // присвоить значение на котором БЫЛ элемент проверки значения предыдущего
                 input[location + 1] = input[location];
                 location = location - 1;  // декрементация локашион
             }
-            input[location+1] = newElement; // значению LOCATION +1 ПЕРЕДАЁМ значение из памяти
+            input[location + 1] = newElement; // значению LOCATION +1 ПЕРЕДАЁМ значение из памяти
         }
         return input;
     }
