694eccd5dfea9ce4ff6d77e795a920b2c12dc41d
dina-03 <dingrafikweb@gmail.com>
2020-08-31 12:33:47 +0200

    lesson-51feinel

--- BC13Java-Morning/src/RecursionExercise.java
+++ BC13Java-Morning/src/RecursionExercise.java
@@ -1,8 +1,54 @@
 public class RecursionExercise {
     public static void main(String[] args) {
-        System.out.println(endX("xxre"));// → "rexx"
+       /* System.out.println(endX("xxre"));// → "rexx"
         System.out.println(endX("xxhixx"));// → "hixxxx"
         System.out.println(endX("xhixhix"));// → "hihixxx"
+        System.out.println(count11("11abc11"));// → 2
+        System.out.println(count11("abc11x11x11"));// → 3
+        System.out.println(count11("111"));// → 1
+        System.out.println(parenBit("xyz(abc)123"));// → "(abc)"
+        System.out.println(parenBit("x(hello)")); //→ "(hello)"
+        System.out.println(parenBit("(xy)1")); //→ "(xy)"*/
+        System.out.println(strCopies("catcowcat", "cat", 2));// → true
+        System.out.println(strCopies("catcowcat", "cow", 2));// → false
+        System.out.println(strCopies("catcowcat", "cow", 1));// → true
+    }
+
+    private static boolean strCopies(String input, String etalon, int count) {
+        if (count == 0) {
+            return true;
+        }
+        if (input.length() < etalon.length()) {
+            return false;
+        }
+        if (input.substring(0, etalon.length()).equals(etalon)) {
+            return strCopies(input.substring(1), etalon, count - 1);
+        }
+        return strCopies(input.substring(1), etalon, count);
+    }
+
+    private static String parenBit(String input) {
+        if (input.length() == 0) {
+            return "";
+        }
+        if (input.charAt(0) != '(') {
+            return parenBit(input.substring(1));
+        } else if (input.charAt(input.length() - 1) != ')') {
+            return parenBit(input.substring(0, input.length() - 1));
+        }
+        return input;
+    }
+
+    private static int count11(String input) {
+        //int count = 0;
+        if (input.length() < 2) {
+            return 0;
+        }
+        if (input.substring(0, 2).equals("11")) {
+            // count++;
+            return 1 + count11(input.substring(2));
+        }
+        return count11(input.substring(1));
     }
 
     private static String endX(String str) {
