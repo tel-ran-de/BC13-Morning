48aefa9a451a84f7e4eb70e33884606973936a98
andron13 <andron13@gmail.com>
2020-09-01 14:41:03 +0200

    Exercises02

--- BC13Java-Morning/src/RecursionExercise.java
+++ BC13Java-Morning/src/RecursionExercise.java
@@ -3,6 +3,10 @@ public class RecursionExercise {
         System.out.println(endX("xxre"));// → "rexx"
         System.out.println(endX("xxhixx"));// → "hixxxx"
         System.out.println(endX("xhixhix"));// → "hihixxx"
+        System.out.println("-----");
+        System.out.println(count11("11abc11"));// → 2
+        System.out.println(count11("abc11x11x11"));// → 3
+        System.out.println(count11("111"));// → 1
         System.out.println(strCopies("catcowcat", "cat", 2));// → true
         System.out.println(strCopies("catcowcat", "cow", 2));// → false
         System.out.println(strCopies("catcowcat", "cow", 1));// → true
@@ -31,4 +35,16 @@ public class RecursionExercise {
         return str.charAt(0) + endX(str.substring(1));
     }
 
+    // Given a string, compute recursively (no loops) the number of “11” substrings in the string.
+    // The “11” substrings should not overlap.
+
+    public static int count11(String input) {
+        if (input.length() < 2) {
+            return 0;
+        }
+        if (input.substring(0, 2).equals("11")) {
+            return 1 + count11(input.substring(2));
+        }
+        return count11(input.substring(1));
+    }
 }
