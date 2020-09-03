120d1212b058bd12115c7306a88d8adcf6890a40
Maryfro <memusena@gmail.com>
2020-09-03 12:43:58 +0200

    try and catch

--- BC13Java-Morning/src/TryAndCry.java
+++ BC13Java-Morning/src/TryAndCry.java
@@ -2,7 +2,26 @@ public class TryAndCry {
     public static void main(String[] args) {
 /*        int[] array = new int[5];
         int element = array[5];*/
-        testArr();
+        //testArr();
+        myParsing();
+    }
+
+    private static void myParsing() {
+        String strOne = "1965";
+        String strTwo = "№123";
+        int numOne = Integer.parseInt(strOne);
+        System.out.println(numOne);
+        try {
+            int numTwo = Integer.parseInt(strTwo);
+            System.out.println(numTwo);
+        } catch (NumberFormatException ex) {
+            System.out.println("exception caught");
+        } finally {
+            System.out.println("nothing happened");
+            System.out.println("use Character.isDigit() and for loop for solving issue");
+        }
+
+
     }
 
     private static void testArr() {
@@ -14,7 +33,7 @@ public class TryAndCry {
             System.out.println("Я НИКОГДА НЕ УВИЖУ ТЕРМИНАЛ");
         } catch (ArrayIndexOutOfBoundsException exception) {
             System.out.println("МЫ ПОЙМАЛИ ОШИБКУ, вылетела ошибка номер 456");
-        }finally {
+        } finally {
             System.out.println("Выполнение завершающего блока, " +
                     "например рестарт программы или шеф всё плохо посмотри чё тута.");
         }
